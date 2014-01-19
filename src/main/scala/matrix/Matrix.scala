914278de3b562679145979a57fe51a7f18b85df3
yingrui <yingrui.f@gmail.com>
2014-01-19 19:24:54 +0800

    add +/- method

--- src/main/scala/matrix/Matrix.scala
+++ src/main/scala/matrix/Matrix.scala
@@ -2,15 +2,28 @@ package matrix
 
 trait Matrix {
 
+  val row: Int
+  val col: Int
+
   def apply(i: Int, j: Int): Double
 
   def update(i: Int, j: Int, value: Double)
 
+  def +(n: Double): Matrix
+
+  def -(n: Double): Matrix
+
+  def == (other: Matrix): Boolean = {
+    val value = for (i <- 0 until row; j <- 0 until col) yield Math.abs(apply(i, j) - other(i, j))
+    row == other.row && col == other.col && value.sum < 1E-10
+  }
 }
 
 object Matrix {
 
   def apply(row: Int, col: Int): Matrix = new DenseMatrix(row, col, new Array[Double](row * col))
+
+  def apply(row: Int, col: Int, elements: Array[Double]): Matrix = new DenseMatrix(row, col, elements)
 
 }
 
@@ -19,4 +32,9 @@ class DenseMatrix(val row: Int, val col: Int, elements: Array[Double]) extends M
   def apply(i: Int, j: Int): Double = elements(i * col + j)
 
   def update(i: Int, j: Int, value: Double): Unit = elements(i * col + j) = value
+
+  def +(n: Double): Matrix = ???
+
+  def -(n: Double): Matrix = ???
+
 }
--- src/test/scala/matrix/MatrixSuite.scala
+++ src/test/scala/matrix/MatrixSuite.scala
@@ -7,11 +7,29 @@ import org.scalacheck._
 
 class MatrixSuite extends FunSuite with Checkers {
 
+  val matrixGen: Gen[Matrix] = {
+    val indexGen = for (
+      i <- Gen.choose(1, 10);
+      j <- Gen.choose(1, 10)
+    ) yield (i, j)
+    indexGen.flatMap(t =>
+      Gen.listOfN(t._1 * t._2, Gen.choose(-100.0, 100.0))
+        .map(list => Matrix(t._1, t._2, list.toArray))
+    )
+  }
+
   test("should update and get element of matrix by index, like m(i, j) = x") {
     val m = Matrix(2, 2)
     check(forAll(Gen.choose(0, 1), Gen.choose(0, 1), Gen.choose(0.0, 100.0)) {
       (i, j, number) => m(i, j) = number
         number == m(i, j)
+    })
+  }
+
+  test("should support +/- operator") {
+    check(forAll(matrixGen, Gen.choose(-100.0, 100.0)) { (m, num) =>
+      val n = m + num
+      m == n - num
     })
   }
 }
